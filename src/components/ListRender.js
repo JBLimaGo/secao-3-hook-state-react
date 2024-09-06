abbeaa983f485bf4e51ba200f2fd450ae43cacb2
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-05 22:12:33 -0300

    Problema de Propriedade Key onde tem que ser criado um id para um registro que será impresso

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -3,11 +3,23 @@ import { useState } from "react";
 const ListRender = () => {
   const [list] = useState(["JB", "Pedro", "Jesus"]);
 
+  const [users] = useState([
+    { id: 1, name: "Jesus", age: 37 },
+    { id: 222222, name: "JB", age: 32 },
+  ]);
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id}, {user.name}, {user.age}
+          </li>
         ))}
       </ul>
     </div>

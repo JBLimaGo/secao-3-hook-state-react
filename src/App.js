14ff1e7442c81c29df6529c66a1c8b5d95eac830
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-07 19:01:50 -0300

    Usando Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 import imagen2 from "./assets/imagen2.jpg";
 import ConditionalRender from "./components/ConditionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
 
 function App() {
+  const name = "JBL";  // Dados dinamicos
+  const [userName] = useState("Lima"); // Passagem de paramento por variavel - State
   return (
     <div className="App">
       <div>
@@ -26,6 +30,10 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+
+      {/* props - passando um objeto para um componente  com isso posso usar todas propriedades do objeto dentro do componente
+         Pode se passar valores diretos <ShowUserName name="Jefferson" /> por variavel <ShowUserName name={name} />  e por State <ShowUserName name={userName} />*/}
+      <ShowUserName name={userName} />
     </div>
   );
 }

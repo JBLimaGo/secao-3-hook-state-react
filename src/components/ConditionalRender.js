2ee044329aca6318bb63b29831bc38d1e6e7efdf
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-07 16:27:40 -0300

    If Ternário Podemos também realizar o if/else no JSX

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 const ConditionalRender = () => {
   const [x] = useState(false);
+
+  const [nane, setname] = useState("Jefferson");
   return (
     <div>
       <h1> Isso será Exibido? </h1>
       {x && <p> Se X for True, Sim! </p>}
       {!x && <p> Agora x é Falso </p>}
+      <h1> if Ternário </h1>
+
+      {nane === "João" ? (
+        <div>
+          <p>O nome é joão</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setname("João")}>Clique aqui!</button>
     </div>
   );
 };
